346bba9cf2904a94fa9ba1fc79ea4dacce4aa121
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 20:37:36 -0300

    update: Adiciona controlador e servico do uptimeRobot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
 import { InsertUserModule } from "@/controllers/user";
 
 @Module({
   imports: [ InsertUserModule ],
-  controllers: [],
-  providers: []
+  controllers: [ AppController ],
+  providers: [ AppService ]
 })
 export class AppModule { }
