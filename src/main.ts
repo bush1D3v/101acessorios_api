e584e779c546d3c0446834c18c126949a1a63aa5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 18:01:22 -0300

    update: Adiciona Cors e documentacao Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "@/app.module";
+import { type OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
+import document from "../swagger.json";
+import cors from "cors";
 
-async function bootstrap() {
+const PORT = process.env.PORT || 3001;
+
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.use(cors());
+
+  SwaggerModule.setup("", app, document as OpenAPIObject);
+
+  await app.listen(PORT);
 }
 bootstrap();
