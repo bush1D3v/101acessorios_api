8f38191ac85915ed51cd30c6ab2e064accee0871
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 18:06:22 -0300

    feat: Cria modelos de dados para testes de usuario

---
+++ tests/models/globalUserTest.models.ts
@@ -0,0 +1,18 @@
+export const validUserTestName: string = "Victor dos Testes";
+export const validUserTestEmail: string = "victorteste@gmail.com";
+export const validUserTestPassword: string = "vtjln123";
+export const invalidUserTestEmail: string = "victorDosTestes@.com";
+
+//* 14 characters
+export const invalidMinLengthTestEmail: string = "vitu@gmail.com";
+
+//* 76 characters
+export const invalidMaxLengthUserTestName: string =
+  "victorVictorVictorVictorVictorVictorVictorVictorVictorVictorVictorVitorVitor";
+
+//* 76 characters
+export const invalidMaxLengthUserTestEmail: string =
+  "victorVictorVictorVictorVictorVictorVictorVictorVictorVictorVictor@gmail.com";
+
+//* 17 characters
+export const invalidMaxLengthUserTestPassword: string = "12345678912345678";
