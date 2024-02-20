9321e0a17cbd297fc78b581667210beb5a320b59
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 20:07:57 -0300

    style: Cria regras de sintaxe ao projeto

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
-  ignorePatterns: ["*.config.js", ".eslintrc.js"],
+  ignorePatterns: ["*.config.js", ".eslintrc.cjs"],
   rules: {
     quotes: ["error", "double"],
     semi: ["error", "always"],
