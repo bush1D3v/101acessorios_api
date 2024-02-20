5fbd0795bc75053014944291022907bc350bdd88
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 18:15:28 -0300

    feat: Cria index exportador de controladores relacionados a entidade de usuario

---
+++ src/controllers/user/index.ts
@@ -0,0 +1,7 @@
+import { InsertUserModule } from "./InsertUser/insertUser.module";
+import { type InsertUserBody } from "./InsertUser/insertUser.body";
+
+export {
+  InsertUserModule,
+  type InsertUserBody
+};
