814413d53afeacaecc6388dcc485508ce020dae2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-20 18:14:20 -0300

    feat: Cria index exportador de modelos de teste

---
+++ tests/models/index.ts
@@ -0,0 +1,22 @@
+import {
+  validUserTestName,
+  validUserTestPassword,
+  validUserTestEmail,
+  invalidUserTestEmail,
+  invalidMinLengthTestEmail,
+  invalidMaxLengthUserTestEmail,
+  invalidMaxLengthUserTestName,
+  invalidMaxLengthUserTestPassword
+} from "./globalUserTest.models";
+
+export {
+  //* userModels
+  validUserTestName,
+  validUserTestPassword,
+  validUserTestEmail,
+  invalidMinLengthTestEmail,
+  invalidUserTestEmail,
+  invalidMaxLengthUserTestEmail,
+  invalidMaxLengthUserTestName,
+  invalidMaxLengthUserTestPassword
+};
